0a40b413e9d7fa6094a0190ab9ffbfca528966c2
Borjan Peovski <51805234+PeovskiB@users.noreply.github.com>
2024-11-25 18:11:07 +0100

    added routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,19 @@
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Layout from "./layout/Layout";
+import NewsPage from "./pages/NewsPage";
 
 function App() {
-
   return (
-    <>
-      <h1>SektaHub</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/news" element={<NewsPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ app/src/layout/Navbar.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Navbar() {
+  return (
+    <nav>
+        <a href="/">Home</a>
+        <a href="/news">News</a>
+    </nav>
+  )
+}
